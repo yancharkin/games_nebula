e809f3c94da40d70afa4aa826bf1cb4c8bea4646
yancharkin <yancharkin@gmail.com>
2017-05-10 20:10:49 +0300

    Improve 'generate_pot.sh'

--- locale/generate_pot.sh
+++ locale/generate_pot.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-xgettext --language=Python --keyword=_ --output=./games_nebula.pot ../*.py ../../*/*/*.py
+
+xgettext --language=Python --keyword=_ --output=./games_nebula.pot \
+../*.py \
+../../games_nebula_goglib_scripts/*/*.py \
+#~ ../../games_nebula_mylib_scripts/*/*.py
