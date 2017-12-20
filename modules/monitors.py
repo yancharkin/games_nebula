11078e59bcaf3b6a101d2bd41603912661a7db6f
yancharkin <yancharkin@gmail.com>
2017-12-20 16:47:28 +0300

    Improvements in monitors detection Monitors detection should work better now both in X11 and Wayland (Wayland solution nothing more than a hack though).

--- modules/monitors.py
+++ modules/monitors.py
@@ -1,3 +1,4 @@
+import os, subprocess, re
 import gi
 gi.require_version('Gdk', '3.0')
 from gi.repository import Gdk
@@ -6,18 +7,52 @@ def get_monitors():
 
     monitors_list = []
 
-    display_manager = Gdk.DisplayManager.get()
-    display = Gdk.DisplayManager.get_default_display(display_manager)
-    n_monitors = display.get_n_monitors()
+    try:
+        x = os.environ['WAYLAND_DISPLAY']
+        session_type = 'wayland'
+    except KeyError:
+        session_type = 'x11'
 
-    for monitor_index in range(n_monitors):
-        monitor = display.get_monitor(monitor_index)
+    if session_type == 'x11':
 
-        model = monitor.get_model()
-        geometry = monitor.get_geometry()
-        monitors_list.append(model + ' ' + str(geometry.width) + 'x' + str(geometry.height))
+        # Use GTK
+        ## Not working with Wayland (?)
 
-        if monitor.is_primary():
-            monitor_primary = model + ' ' + str(geometry.width) + 'x' + str(geometry.height)
+        display_manager = Gdk.DisplayManager.get()
+        display = Gdk.DisplayManager.get_default_display(display_manager)
+        n_monitors = display.get_n_monitors()
+
+        for monitor_index in range(n_monitors):
+            monitor = display.get_monitor(monitor_index)
+
+            model = monitor.get_model()
+            geometry = monitor.get_geometry()
+            monitors_list.append(model + ' ' + str(geometry.width) + 'x' + str(geometry.height))
+
+            if monitor.is_primary():
+                monitor_primary = model + ' ' + str(geometry.width) + 'x' + str(geometry.height)
+
+    else:
+
+        # Using xrandr
+
+        proc = subprocess.Popen(['xrandr'],stdout=subprocess.PIPE)
+        for line in proc.stdout.readlines():
+
+            if re.compile(r'\b({0})\b'.format('connected'), flags=re.IGNORECASE).search(line):
+                if 'primary' in line:
+                    monitors_list.append(line.split(' ')[0] + ' ' + line.split(' ')[3].split('+')[0])
+                else:
+                    monitors_list.append(line.split(' ')[0] + ' ' + line.split(' ')[2].split('+')[0])
+            if 'primary' in line:
+                monitor_primary = line.split(' ')[0] + ' ' + line.split(' ')[3].split('+')[0]
+
+        ## Hack for Wayland
+        try:
+            monitor_primary
+        except NameError:
+            monitor_primary = monitors_list[0]
+        else:
+            pass
 
     return monitors_list, monitor_primary
