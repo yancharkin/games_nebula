733a3faa95ecb52a49d06629b8410ffe1570ae32
Ivan Yancharkin <yancharkin@gmail.com>
2019-06-20 11:24:08 +0400

    Improve extractor.py. Cleanup

--- extractor.py
+++ extractor.py
@@ -9,7 +9,7 @@ from gi.repository import Gtk
 nebula_dir = os.getenv('NEBULA_DIR')
 
 def simple_message(message_type, text_1, text_2):
-
+    """Show simple message box"""
     message_dialog = Gtk.MessageDialog(
         None,
         0,
@@ -37,86 +37,64 @@ def simple_message(message_type, text_1, text_2):
     message_dialog.run()
     message_dialog.destroy()
 
-def get_files_info(dest_dir):
+def ext_innounp(file_path, dest_dir):
+    """Unpack installer using innounp and wine"""
+    def get_files_info(dest_dir):
+        if sys.version_info[0] == 3:
+            with open(dest_dir + 'install_script.iss', 'r', encoding='utf-8') as f:
+                raw_data = f.read().splitlines()
+        else:
+            with open(dest_dir + 'install_script.iss', 'r') as f:
+                raw_data = f.read().splitlines()
 
-    if sys.version_info[0] == 3:
-        with open(dest_dir + 'install_script.iss', 'r', encoding='utf-8') as f:
-            raw_data = f.read().splitlines()
-    else:
-        with open(dest_dir + 'install_script.iss', 'r') as f:
-            raw_data = f.read().splitlines()
+        files_dict = {}
+        chunks_dict = {}
+        files_chunks_dict = {}
+        for i in range(len(raw_data)):
+            if ('Source: ' in raw_data[i]) and ('BeforeInstall: ' in raw_data[i]):
+                temp_list = raw_data[i].split('; ')
+                file_src = temp_list[0].split(': ')[1].replace('"', '').replace('\\', '/')
+                file_dest = temp_list[3].split(': ')[1].replace('"', '').split(', ')[1].replace("'", '')
+                files_dict[file_dest] = file_src
+                chunks_n = int(temp_list[3].split(': ')[1].replace('"', '').split(', ')[2].strip(')'))
+                if chunks_n != 1:
+                    chunks_list = []
+                    for j in range(1, chunks_n):
+                        temp_list = raw_data[i+j].split('; ')
+                        chunk_src = temp_list[0].split(': ')[1].replace('"', '').replace('\\', '/')
+                        chunk_dest = temp_list[3].split(': ')[1].split(', ')[0].replace('"after_install(', '').replace('"after_install_dependency(', '').replace("'", '')
+                        chunks_dict[chunk_dest] = chunk_src
+                        chunks_list.append(chunk_dest)
+                    files_chunks_dict[file_dest] = chunks_list
 
-    files_dict = {}
-    chunks_dict = {}
-    files_chunks_dict = {}
+        return files_dict, chunks_dict, files_chunks_dict
 
-    for i in range(len(raw_data)):
-
-        if ('Source: ' in raw_data[i]) and ('BeforeInstall: ' in raw_data[i]):
-
-            temp_list = raw_data[i].split('; ')
-            file_src = temp_list[0].split(': ')[1].replace('"', '').replace('\\', '/')
-            file_dest = temp_list[3].split(': ')[1].replace('"', '').split(', ')[1].replace("'", '')
-            files_dict[file_dest] = file_src
-
-            chunks_n = int(temp_list[3].split(': ')[1].replace('"', '').split(', ')[2].strip(')'))
-
-            if chunks_n != 1:
-
-                chunks_list = []
-
-                for j in range(1, chunks_n):
-                    temp_list = raw_data[i+j].split('; ')
-                    chunk_src = temp_list[0].split(': ')[1].replace('"', '').replace('\\', '/')
-                    chunk_dest = temp_list[3].split(': ')[1].split(', ')[0].replace('"after_install(', '').replace('"after_install_dependency(', '').replace("'", '')
-                    chunks_dict[chunk_dest] = chunk_src
-                    chunks_list.append(chunk_dest)
-
-                files_chunks_dict[file_dest] = chunks_list
-
-    return files_dict, chunks_dict, files_chunks_dict
-
-def decompress_files(files_dict, dest_dir):
-
-    for file_path in files_dict:
-
-        full_file_path = dest_dir + file_path
-        file_path_dir = os.path.dirname(full_file_path)
-        file_name = os.path.basename(full_file_path)
-        src_path = dest_dir + files_dict[file_path]
-
-        print('Decompressing: ' + file_name)
-
-        if not os.path.exists(file_path_dir):
-            os.makedirs(file_path_dir)
-
-        with open(src_path, 'rb') as comp_file, \
-                open(full_file_path, 'wb') as decomp_file:
-
-            comp_data = comp_file.read()
-            decomp_data = zlib.decompress(comp_data)
-            decomp_file.write(decomp_data)
-
-def append_chunks(files_chunks_dict, dest_dir):
-
-    for file_path in files_chunks_dict:
-
-        for chunk in files_chunks_dict[file_path]:
-
-            print('Appending chunk to: ' + os.path.basename(file_path))
-
+    def decompress_files(files_dict, dest_dir):
+        for file_path in files_dict:
             full_file_path = dest_dir + file_path
-            full_chunk_path = dest_dir + chunk
+            file_path_dir = os.path.dirname(full_file_path)
+            file_name = os.path.basename(full_file_path)
+            src_path = dest_dir + files_dict[file_path]
+            print('Decompressing: ' + file_name)
+            if not os.path.exists(file_path_dir):
+                os.makedirs(file_path_dir)
+            with open(src_path, 'rb') as comp_file, \
+                    open(full_file_path, 'wb') as decomp_file:
+                comp_data = comp_file.read()
+                decomp_data = zlib.decompress(comp_data)
+                decomp_file.write(decomp_data)
 
-            with open(full_chunk_path, 'rb') as chunk_file, \
-                    open(full_file_path, 'ab') as cur_file:
-
-                chunk = chunk_file.read()
-                cur_file.write(chunk)
-
-            os.remove(full_chunk_path)
-
-def innounp(file_path, dest_dir):
+    def append_chunks(files_chunks_dict, dest_dir):
+        for file_path in files_chunks_dict:
+            for chunk in files_chunks_dict[file_path]:
+                print('Appending chunk to: ' + os.path.basename(file_path))
+                full_file_path = dest_dir + file_path
+                full_chunk_path = dest_dir + chunk
+                with open(full_chunk_path, 'rb') as chunk_file, \
+                        open(full_file_path, 'ab') as cur_file:
+                    chunk = chunk_file.read()
+                    cur_file.write(chunk)
+                os.remove(full_chunk_path)
 
     innounp = nebula_dir + '/bin/innounp.exe'
     os.environ['WINEDEBUG'] = '-all'
@@ -146,33 +124,31 @@ def innounp(file_path, dest_dir):
     if os.path.exists(game_dir): os.rename(game_dir, game_dir_new)
     if os.path.exists(tmp_dir): os.rename(tmp_dir, tmp_dir_new)
 
-def innoextact(file_path, dest_dir):
-
+def ext_innoextact(file_path, dest_dir):
+    """Unpack installer using innoextract"""
     if os.path.exists(nebula_dir + '/bin/innoextract'):
         innoextract = nebula_dir + '/bin/innoextract'
     else:
         innoextract = 'innoextract'
-
     subprocess.call([innoextract, '--gog', file_path, '-d', dest_dir])
 
-def wine(file_path, dest_dir):
-
+def ext_wine(file_path, dest_dir):
+    """Unpack installer using wine"""
     wineprefix_path = dest_dir + '/wine_prefix'
     os.environ['WINEPREFIX'] = wineprefix_path
-    #os.environ['WINEDEBUG'] = '-all'
+    os.environ['WINEDEBUG'] = '-all'
     os.environ['WINEDLLOVERRIDES'] = 'mshtml,mscoree=d'
     if not os.path.exists(wineprefix_path):
         os.makedirs(wineprefix_path)
     dest_dir = dest_dir + '/game'
     proc = subprocess.Popen(['winepath', '-w', dest_dir], stdout=subprocess.PIPE)
     dest_dir_win = proc.stdout.readline().decode('utf-8').strip()
-
     subprocess.call([
-        'wine',
-        file_path,
-        '/DIR=' + dest_dir_win,
-        '/VERYSILENT',
-        '/SUPPRESSMSGBOXES'
+            'wine',
+            file_path,
+            '/DIR=' + dest_dir_win,
+            '/VERYSILENT',
+            '/SUPPRESSMSGBOXES'
     ])
     if not os.path.exists(dest_dir):
         simple_message(
@@ -183,62 +159,28 @@ def wine(file_path, dest_dir):
         while Gtk.events_pending():
             Gtk.main_iteration_do(False)
         subprocess.call([
-            'wine',
-            file_path,
-            '/LANG=english',
-            '/DIR=' + dest_dir_win,
+                'wine',
+                file_path,
+                '/LANG=english',
+                '/DIR=' + dest_dir_win,
         ])
 
-#    # Multi-part installers or some installers (created with particular inno setup version) can't be installed silently
-#    simple_message(
-#        Gtk.MessageType.INFO,
-#        "Info",
-#        "Do not change the installation path!"
-#    )
-#    while Gtk.events_pending():
-#        Gtk.main_iteration_do(False)
-#    subprocess.call([
-#        'wine',
-#        file_path,
-#        '/LANG=english',
-#        '/DIR=' + dest_dir_win,
-#    ])
-#
-#    #installer_dir = os.path.dirname(file_path)
-#    #installer_files_n = len(os.listdir(installer_dir))
-#    #if installer_files_n == 1:
-#    #    # Good solution, completely silent installation
-#    #    subprocess.call([
-#    #        'wine',
-#    #        file_path,
-#    #        '/DIR=' + dest_dir_win,
-#    #        '/VERYSILENT',
-#    #        '/SUPPRESSMSGBOXES'
-#    #    ])
-#    #else:
-#    #    # Multi-part installers (if that's the problem)
-#    #    simple_message(
-#    #        Gtk.MessageType.INFO,
-#    #        "Info",
-#    #        "Do not change the installation path!"
-#    #    )
-#    #    while Gtk.events_pending():
-#    #        Gtk.main_iteration_do(False)
-#    #    subprocess.call([
-#    #        'wine',
-#    #        file_path,
-#    #        '/LANG=english',
-#    #        '/DIR=' + dest_dir_win,
-#    #    ])
+def ext_unzip(file_path, dest_dir):
+    """Unpack installer using unzip"""
+    subprocess.call([
+            'unzip', '-o',
+            file_path,
+            '-d',
+            dest_dir
+    ])
 
 def extract(file_path, dest_dir, *args):
-    #if dest_dir[-1] != '/':
-    #    dest_dir += '/'
+    """Main function"""
     installer_type = args[0][0]
     if installer_type == 'exe':
-        wine(file_path, dest_dir)
+        ext_wine(file_path, dest_dir)
     elif installer_type == 'sh':
-        print('Exract using unzip')
+        ext_unzip(file_path, dest_dir)
 
 if __name__ == '__main__':
     import sys
